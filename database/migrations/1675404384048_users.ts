77cf299fb72c02a42d35a6ce51ba9fbf35ba8ffe
Geriano <gerznewbie@gmail.com>
2023-02-03 19:16:08 +0700

    change to big increment

--- database/migrations/1675404384048_users.ts
+++ database/migrations/1675404384048_users.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
+      table.bigIncrements('id').primary()
       table.string('name')
       table.string('email', 255).notNullable().unique()
       table.string('username', 64).notNullable().unique()
